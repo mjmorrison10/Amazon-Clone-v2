adafc8b05e1d0649bb33525ab7636e253f2d3455
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-08-02 17:38:24 -0700

    Update cart.js

--- cart.js
+++ cart.js
@@ -184,8 +184,8 @@ function generateCartItems(cartItems) {
     let itemsHTML = "";
     cartItems.forEach((item) => {
         itemsHTML += `
-            <div class="cart-item flex items-center pb-4 border-b border-gray-100">
-                <div class="cart-item-image w-40 h-24 bg-white p-4 rounded-lg">
+            <div class="cart-item flex items-center border-b border-gray-100">
+                <div class="cart-item-image m-2 w-40 h-24 bg-white p-4 rounded-lg">
                     <img class="w-full h-full object-contain" src="${item.image}">
                 </div>
                 <div class="cart-item-details flex-grow">
